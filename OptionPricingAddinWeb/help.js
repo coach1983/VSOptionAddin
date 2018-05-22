ca8309ece977da70cb612d0a3d870ed56f8f0baf
coach1983 <eric.j.ramos@gmail.com>
2018-05-22 13:59:56 -0700

    Help files

---
+++ OptionPricingAddinWeb/help.js
@@ -0,0 +1,19 @@
+﻿'use strict';
+
+
+(function () {
+    Office.initialize = function (reason) {
+        $(document).ready(function () {
+
+            if (!Office.context.requirements.isSetSupported('ExcelApi', 1.7)) {
+                console.log('Sorry. The tutorial add-in uses Excel.js APIs that are not available in your version of Office.');
+            }
+
+           
+           
+
+        });
+    };
+
+
+})();
